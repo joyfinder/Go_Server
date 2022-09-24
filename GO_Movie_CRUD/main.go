57fb7b3037ecc6484e3690fcd25da784040bd7fe
Ta-Liang Lu <ste176876@gmail.com>
2022-09-24 10:41:21 +0800

    fix type of length & add getMovies func

--- GO_Movie_CRUD/main.go
+++ GO_Movie_CRUD/main.go
@@ -1,13 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 type Movie struct {
-	ID       string    `json:"id"`
-	Name     string    `json:"name"`
-	Origin   string    `json:"origin"`
-	Length   int       `json:"length"`
-	Producer *Producer `json:"producer"`
+	ID           string    `json:"id"`
+	Name         string    `json:"name"`
+	Origin       string    `json:"origin"`
+	Length_Hours float32   `json:"length_hours"`
+	Producer     *Producer `json:"producer"`
 }
 
 type Producer struct {
@@ -17,10 +21,16 @@ type Producer struct {
 
 var movies []Movie
 
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "applicaiton/json")
+	json.NewEncoder(w).Encode(movies)
+}
+
 func main() {
 	fmt.Print("Welcome to Movie intro!")
 	r := mux.NewRouter()
 
+	movies = append(movies, Movie{ID: "01", Name: "Narco", Origin: "SK", Length_Hours: 2.5, Producer: &Producer{FirstName: "Kim", LastName: "Tony"}})
 	r.HandleFunc("/movies", getMovies).Method("GET")
 	r.HandleFunc("/movies/{id}", getMovie).Method("GET")
 	r.HandleFunc("/movies", createMovie).Method("POST")
