ff621114383d8dc67f9bc26cf8a46b158708e0a7
Ta-Liang Lu <ste176876@gmail.com>
2022-09-24 10:22:29 +0800

    Build producer struct

--- GO_Movie_CRUD/main.go
+++ GO_Movie_CRUD/main.go
@@ -3,11 +3,16 @@ package main
 import "fmt"
 
 type Movie struct {
-	ID       string `json:"id"`
-	Name     string `json:"name"`
-	Origin   string `json:"origin"`
-	Length   int    `json:"length"`
-	Producer string `json:"producer"`
+	ID       string    `json:"id"`
+	Name     string    `json:"name"`
+	Origin   string    `json:"origin"`
+	Length   int       `json:"length"`
+	Producer *Producer `json:"producer"`
+}
+
+type Producer struct {
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
 }
 
 func main() {
