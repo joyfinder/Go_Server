62b74f73dd5eeca56253d9bf156455710c5e681e
Ta-Liang Lu <ste176876@gmail.com>
2022-09-24 10:20:22 +0800

    Create movie struct

--- GO_Movie_CRUD/main.go
+++ GO_Movie_CRUD/main.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+type Movie struct {
+	ID       string `json:"id"`
+	Name     string `json:"name"`
+	Origin   string `json:"origin"`
+	Length   int    `json:"length"`
+	Producer string `json:"producer"`
+}
+
 func main() {
 	fmt.Print("Hello")
 }
