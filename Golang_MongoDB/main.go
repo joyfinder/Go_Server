68006973ccf9df6c33eebc44b00eaa1e7034add1
Ta-Liang Lu <ste176876@gmail.com>
2022-09-26 19:34:38 +0800

    Create getSession method

--- Golang_MongoDB/main.go
+++ Golang_MongoDB/main.go
@@ -1,7 +1,25 @@
 package main
 
+import (
+	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2"
+)
+
 func main() {
 
 	r := httprouter.New()
 	uc := controllers.NewUserController(getSession())
+	r.GET("")
+	r.POST("")
+	r.DELETE("")
+}
+
+func getSession() *mgo.Session {
+
+	// Helps connect MongoDB
+	s, err := mgo.Dial("mongodb://localhost:28088")
+	if err != nil {
+		panic(err)
+	}
+	return s
 }
