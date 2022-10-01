1483b124999d65b494e2c9b2c42862d031caa315
Ta-Liang Lu <ste176876@gmail.com>
2022-10-01 10:10:36 +0800

    Create render receiver

--- GO_To_Do_List/main.go
+++ GO_To_Do_List/main.go
@@ -99,6 +99,9 @@ func fetchTodos(w http.ResponseWriter, r *http.Request){
 			CreatedAt: t.CreatedAt,
 		})
 	}
+	rnd.JSON(w, http.StatusOK, renderer.M{
+		"data": todoList,
+	})
 }
 
 func homeHandler() http.Handler {
