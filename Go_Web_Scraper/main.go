b05ecb6659f814e19011a4097d2633b08d252023
Ta-Liang Lu <ste176876@gmail.com>
2022-10-04 19:23:41 +0800

    Create struct of search result & bing domains

---
+++ Go_Web_Scraper/main.go
@@ -0,0 +1,16 @@
+package main
+
+
+bingDomains = map[string]string{
+	"com":""
+}
+
+type Search_Result struct{
+	Result_rank int
+	ResultURL string
+	Result_title string
+	Result_desc string
+}
+func main() {
+
+}
