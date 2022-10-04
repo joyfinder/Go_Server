2958322d97a621b665719041091d5a149abc1a7c
Ta-Liang Lu <ste176876@gmail.com>
2022-10-04 19:29:15 +0800

    Create framework of func

--- Go_Web_Scraper/main.go
+++ Go_Web_Scraper/main.go
@@ -5,11 +5,34 @@ bingDomains = map[string]string{
 	"com":""
 }
 
+var userAgents = []string{
+
+}
 type Search_Result struct{
 	Result_rank int
 	ResultURL string
 	Result_title string
 	Result_desc string
+}
+
+func randomUserAgent() string{
+
+}
+
+func buildBingUrls() {
+
+}
+
+func scrapeClientRequest() {
+
+}
+
+func bingScrape(){
+
+}
+
+func bingResultParser(){
+	
 }
 func main() {
 
