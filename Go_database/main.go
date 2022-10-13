560e9fc9ee3d053bdf78b0cdb4e906c1a8a3a6f0
Ta-Liang Lu <ste176876@gmail.com>
2022-10-13 22:19:25 +0800

    Create number of employees data

--- Go_database/main.go
+++ Go_database/main.go
@@ -26,4 +26,13 @@ func main() {
 	if err != nil {
 		fmt.Println("Error", err)
 	}
+
+	employees := []User{
+		{"John", "23", "2334333", "Real", Address{"indo", "asd", "thai", "120381"}},
+		{"Alice", "23", "2334333", "Fake comp", Address{"indo", "asd", "thai", "112804"}},
+		{"Annie", "23", "2334333", "MSFT", Address{"indo", "asd", "thai", "1201924"}},
+		{"Charlie", "23", "2334333", "Meta", Address{"indo", "asd", "thai", "1212412"}},
+		{"Steph", "23", "2334333", "Google", Address{"indo", "asd", "thai", "15235524"}},
+		{"Even", "23", "2334333", "Unicorn", Address{"indo", "asd", "thai", "12124"}},
+	}
 }
