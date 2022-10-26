01748903d7d8f4f4577ed8a4e614045c44befb98
Ta-Liang Lu <ste176876@gmail.com>
2022-10-26 20:10:00 +0800

    Create orderRouter

---
+++ Go_restaurant/routes/orderRouter.go
@@ -0,0 +1,8 @@
+package routes
+
+func OrderRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.GET("/orders", controller.GetOrders())
+	incomingRoutes.GET("/orders/:order_id", controller.GetOrder())
+	incomingRoutes.POST("/orders", controller.CreateOrder())
+	incomingRoutes.PATCH("/orders/:order", controller.UpdateOrder())
+}
