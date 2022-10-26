a56b4fc6b279278309c43e8f8e4770f36755c48b
Ta-Liang Lu <ste176876@gmail.com>
2022-10-26 20:08:38 +0800

    Import controller lib

--- Go_restaurant/routes/invoiceRouter.go
+++ Go_restaurant/routes/invoiceRouter.go
@@ -1,5 +1,8 @@
 package routes
 
+import (
+	controller "Go_restaurant/controllers"
+)
 func InvoiceRoutes(incomingRoutes *gin.Engine) {
 	incomingRoutes.GET("/invoices", controller.GetInvoices()))
 	incomingRoutes.GET("/invoices/:invoice_id", controller.GetInvoice())
--- Go_restaurant/routes/menuRouter.go
+++ Go_restaurant/routes/menuRouter.go
@@ -1,5 +1,9 @@
 package routes
 
+import (
+	controller "Go_restaurant/controllers"
+)
+
 func MenuRoutes(incomingRoutes *gin.Engine) {
 	incomingRoutes.GET("/menus", controller.GetMenus())
 	incomingRoutes.GET("/menus/:menu_id", controller.GetMenu())
