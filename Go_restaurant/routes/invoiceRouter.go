4e2ea08df14e64253c30b3574507b5b802f2212a
Ta-Liang Lu <ste176876@gmail.com>
2022-10-26 20:05:24 +0800

    Create api for invoiceRouter

--- Go_restaurant/routes/invoiceRouter.go
+++ Go_restaurant/routes/invoiceRouter.go
@@ -1,5 +1,8 @@
 package routes
 
 func InvoiceRoutes(incomingRoutes *gin.Engine) {
-
+	incomingRoutes.GET("/invoices", controller.GetInvoices()))
+	incomingRoutes.GET("/invoices/:invoice_id", controller.GetInvoice())
+	incomingRoutes.POST("/invoices", controller.CreateInvoice())
+	incomingRoutes.PATCH("/invoices/:invoice", controller.UpdateInvoice())
 }
