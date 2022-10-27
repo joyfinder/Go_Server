7c6849f5651f8cfbdfd0d1a3cd68e1437a01666e
Ta-Liang Lu <ste176876@gmail.com>
2022-10-27 21:10:40 +0800

    Create another API for orderItem get

--- Go_restaurant/routes/orderItermRouter.go
+++ Go_restaurant/routes/orderItermRouter.go
@@ -7,6 +7,7 @@ import (
 func OrderItemRoutes(incomingRoutes *gin.Engine) {
 	incomingRoutes.GET("/orderItems", controller.GetOrderItems())
 	incomingRoutes.GET("/orderItems/:orderItem_id", controller.GetOrderItem())
+	incomingRoutes.GET("/orderItemso-order/:order_id", controller.GetOrderItemsByOrder())
 	incomingRoutes.POST("/orderItems", controller.CreateOrderItem())
 	incomingRoutes.PATCH("/orderItems/:orderItem", controller.UpdateOrderItem())
 }
