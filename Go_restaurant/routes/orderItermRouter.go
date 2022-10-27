e42067f4d621a8542b7db947b289b5a4d7ce3426
Ta-Liang Lu <ste176876@gmail.com>
2022-10-27 21:09:02 +0800

    Create tableRouter & orderItemRouter

---
+++ Go_restaurant/routes/orderItermRouter.go
@@ -0,0 +1,12 @@
+package routes
+
+import (
+	controller "Go_restaurant/controllers"
+)
+
+func OrderItemRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.GET("/orderItems", controller.GetOrderItems())
+	incomingRoutes.GET("/orderItems/:orderItem_id", controller.GetOrderItem())
+	incomingRoutes.POST("/orderItems", controller.CreateOrderItem())
+	incomingRoutes.PATCH("/orderItems/:orderItem", controller.UpdateOrderItem())
+}
