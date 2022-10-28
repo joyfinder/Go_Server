2b28ca31b3f73f513088b7d7874802e4be2b8212
Ta-Liang Lu <ste176876@gmail.com>
2022-10-28 20:07:24 +0800

    Create error exception

--- Go_restaurant/database/databaseConnection.go
+++ Go_restaurant/database/databaseConnection.go
@@ -1,10 +1,16 @@
 package database
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
 func DBinstance() *mongo.Client {
 	MongoDB := "mongodb://localhost:27017"
 	fmt.Print(MongoDB)
 
 	client, err := mongo.NewClient(options.Client().ApplyURI(MongoDB))
+	if err != nil {
+		log.Fatal(err)
+	}
 }
