6f9d150d208eec6eb6a8dea213dc80e50057b223
Ta-Liang Lu <ste176876@gmail.com>
2022-10-29 13:52:10 +0800

    Create GetUser & GetUsers func

---
+++ Go_restaurant/controllers/userController.go
@@ -0,0 +1,15 @@
+package controller
+
+import "github.com/gin-gonic/gin"
+
+func GetUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func GetUsers() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
