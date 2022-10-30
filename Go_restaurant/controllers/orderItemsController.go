c14192c7eac59ec06d297f7ceecc6a95a10b07e1
Ta-Liang Lu <ste176876@gmail.com>
2022-10-30 09:22:06 +0800

    Create Item by order func

--- Go_restaurant/controllers/orderItemsController.go
+++ Go_restaurant/controllers/orderItemsController.go
@@ -20,6 +20,10 @@ func CreateOrderItem() gin.HandlerFunc {
 	}
 }
 
+func ItemsByOrders(id string) (OrderItems []primitive.M, err error) {
+
+}
+
 func UpdateOrderItem() gin.HandlerFunc {
 	return func(c *gin.Context) {
 
