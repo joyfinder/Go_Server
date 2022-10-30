aee681e52248c3050696b432fa0cf19690bcd49d
Ta-Liang Lu <ste176876@gmail.com>
2022-10-30 09:32:20 +0800

    Fix menu_id struct

--- Go_restaurant/models/foodModel.go
+++ Go_restaurant/models/foodModel.go
@@ -14,5 +14,5 @@ type Food struct {
 	Created_at time.Time          `json:"created_at"`
 	Updated_at time.Time          `json:"updated_at"`
 	food_id    string             `json:"food_id"`
-	Menu_id    *string            `json:"menu_id"`
+	Menu_id    *string            `json:"menu_id" validate:"required"`
 }
