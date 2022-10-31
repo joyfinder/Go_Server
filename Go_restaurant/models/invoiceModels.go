d8bf974ab0320c1e9addb186cc837ecb377d6f2a
Ta-Liang Lu <ste176876@gmail.com>
2022-10-31 21:20:12 +0800

    Assign types into invoice struct

--- Go_restaurant/models/invoiceModels.go
+++ Go_restaurant/models/invoiceModels.go
@@ -12,7 +12,7 @@ type Invoice struct {
 	Order_id         string             `json:"order_id"`
 	Payment_method   *string            `json:"payment_method" validate:"eq=CARD|eq=CASH|eq="`
 	Payment_status   *string            `json:"payment_status" validate:"required,eq=PENDING|eq=PAID"`
-	Payment_due_date time.Time
-	Created_at       time.Time
-	Updated_at       time.Time
+	Payment_due_date time.Time          `json:"payment_due_date"`
+	Created_at       time.Time          `json:"created_at"`
+	Updated_at       time.Time          `json:"updated_at"`
 }
