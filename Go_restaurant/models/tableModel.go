961f677c88d4c0e2a0b4a5a2ab95dfe223e0dc00
Ta-Liang Lu <ste176876@gmail.com>
2022-11-01 15:27:11 +0800

    Add table struct

--- Go_restaurant/models/tableModel.go
+++ Go_restaurant/models/tableModel.go
@@ -8,9 +8,9 @@ import (
 
 type Table struct {
 	ID               primitive.ObjectID `bson:`
-	Number_of_guests *int
-	Table_number     *int
-	Created_at       time.Time `json:"created_at"`
-	Updated_at       time.Time `json:"updated_at"`
-	Table_id         string    `json:"table_id" validate:"required"`
+	Number_of_guests *int               `json:"number_of_guests validate:"required"`
+	Table_number     *int               `json:"table_number" validate:"required"`
+	Created_at       time.Time          `json:"created_at"`
+	Updated_at       time.Time          `json:"updated_at"`
+	Table_id         string             `json:"table_id" validate:"required"`
 }
