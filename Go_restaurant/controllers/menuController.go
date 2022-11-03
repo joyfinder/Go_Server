24ecb94888ab14bc35271e0c909833801747b1bd
Ta-Liang Lu <ste176876@gmail.com>
2022-11-03 18:46:47 +0800

    Create menu collection in menu controller

--- Go_restaurant/controllers/menuController.go
+++ Go_restaurant/controllers/menuController.go
@@ -1,10 +1,17 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+
+	"github.com/gin-gonic/gin"
+	"gopkg.in/mgo.v2/bson"
+)
+
+var menuCollection *mongo.Collection = database.OpenCollection(database.Client, "menu")
 
 func GetMenus() gin.HandlerFunc {
 	return func(c *gin.Context) {
-
+		result, err := menuCollection.Find(context.TODO(), bson.M{})
 	}
 }
 
