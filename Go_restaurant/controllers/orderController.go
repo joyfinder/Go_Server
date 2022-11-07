eb651f7cf2fe813228f03dd015df33fe74beb028
Ta-Liang Lu <ste176876@gmail.com>
2022-11-07 16:17:56 +0800

    Produce result & err var & free its resources

--- Go_restaurant/controllers/orderController.go
+++ Go_restaurant/controllers/orderController.go
@@ -2,14 +2,23 @@ package controllers
 
 import (
 	"context"
+	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"gopkg.in/mgo.v2/bson"
 )
 
 func GetOrders() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+		result, err := orderCollection.Find(context.TODO(), bson.M{})
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occurred when listing items."})
+		}
+
 	}
 }
 
