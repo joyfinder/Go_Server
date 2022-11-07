da16c097962e5cabb40ea7b78a3e8ccea684b577
Ta-Liang Lu <ste176876@gmail.com>
2022-11-07 16:23:52 +0800

    Replicate GetFood func from food controller to order controller

--- Go_restaurant/controllers/orderController.go
+++ Go_restaurant/controllers/orderController.go
@@ -29,7 +29,16 @@ func GetOrders() gin.HandlerFunc {
 
 func GetOrder() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		orderId := c.Param("order_id")
+		var order models.Order
 
+		err := foodCollection.FindOne(ctx, bson, M{"food_id": orderId}).Decode(&order)
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occurred while ordering the food"})
+		}
+		c.JSON(http.StatusOK, order)
 	}
 }
 
