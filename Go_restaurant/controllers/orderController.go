a1a1e1d5916ea8c8f241d08d30c49f22d1dc4a8d
Ta-Liang Lu <ste176876@gmail.com>
2022-11-07 16:09:04 +0800

    Initiate context & cancel var

--- Go_restaurant/controllers/orderController.go
+++ Go_restaurant/controllers/orderController.go
@@ -1,10 +1,15 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
 
 func GetOrders() gin.HandlerFunc {
 	return func(c *gin.Context) {
-
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 	}
 }
 
