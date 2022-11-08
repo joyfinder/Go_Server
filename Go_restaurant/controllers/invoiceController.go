690c0a82c1cb39e211a5ad6849e61125bdbff7d9
Ta-Liang Lu <ste176876@gmail.com>
2022-11-08 20:01:15 +0800

    Assign types into invoice struct

--- Go_restaurant/controllers/invoiceController.go
+++ Go_restaurant/controllers/invoiceController.go
@@ -1,16 +1,20 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
 
 type InvoiceViewFormat struct {
-	Invoice_id
-	Payment_method
-	Order_id
-	Payment_status
-	Payment_due
-	Table_number
-	Payment_due_date
-	Order_details
+	Invoice_id       string
+	Payment_method   string
+	Order_id         string
+	Payment_status   *string
+	Payment_due      interface{}
+	Table_number     interface{}
+	Payment_due_date time.Time
+	Order_details    interface{}
 }
 
 func GetInvoices() gin.HandlerFunc {
