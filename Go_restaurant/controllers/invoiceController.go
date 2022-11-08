83a96dc3dc18fe1edcabf110e64e2cf37e2d828b
Ta-Liang Lu <ste176876@gmail.com>
2022-11-08 20:00:23 +0800

    Construct InvoiceViewFormat struct

--- Go_restaurant/controllers/invoiceController.go
+++ Go_restaurant/controllers/invoiceController.go
@@ -2,6 +2,17 @@ package controllers
 
 import "github.com/gin-gonic/gin"
 
+type InvoiceViewFormat struct {
+	Invoice_id
+	Payment_method
+	Order_id
+	Payment_status
+	Payment_due
+	Table_number
+	Payment_due_date
+	Order_details
+}
+
 func GetInvoices() gin.HandlerFunc {
 	return func(c *gin.Context) {
 
