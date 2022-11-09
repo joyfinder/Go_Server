599575564f26ca318e84f40ace1a7a66eca9827e
Ta-Liang Lu <ste176876@gmail.com>
2022-11-09 18:23:46 +0800

    Fix typo of invoiceCollection

--- Go_restaurant/controllers/invoiceController.go
+++ Go_restaurant/controllers/invoiceController.go
@@ -26,7 +26,7 @@ var invoiceCollection *mongo.Colleciton = database.OpenCollection(database.Clien
 func GetInvoices() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
-		result, err := orderCollection.Find(context.TODO(), bson.M{})
+		result, err := invoiceCollection.Find(context.TODO(), bson.M{})
 		defer cancel()
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occurred when getting invoices."})
