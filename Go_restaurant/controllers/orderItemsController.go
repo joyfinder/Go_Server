402fe003ff0321b43ac71e4ad16ccb4fa839d454
Ta-Liang Lu <ste176876@gmail.com>
2022-11-11 09:24:49 +0800

    Initiate order item pack & item collection

--- Go_restaurant/controllers/orderItemsController.go
+++ Go_restaurant/controllers/orderItemsController.go
@@ -2,6 +2,13 @@ package controllers
 
 import "github.com/gin-gonic/gin"
 
+type OrderItemPack struct {
+	Table_id    *string
+	Order_items []models.OrderItem
+}
+
+var orderItemCollection *mongo.Collection = database.OpenCollection(database.Client, "orderItem")
+
 func GetOrderItemsByOrder() gin.HandlerFunc {
 	return func(c *gin.Context) {
 
