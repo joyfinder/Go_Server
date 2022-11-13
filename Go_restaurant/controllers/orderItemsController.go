157f914d263514bbfbc2d9220c62a6eb727026d3
Ta-Liang Lu <ste176876@gmail.com>
2022-11-13 23:01:21 +0800

    Initiate ctx, cancel & orderItem

--- Go_restaurant/controllers/orderItemsController.go
+++ Go_restaurant/controllers/orderItemsController.go
@@ -99,6 +99,7 @@ func ItemsByOrder(id string) (OrderItems []primitive.M, err error) {
 
 func UpdateOrderItem() gin.HandlerFunc {
 	return func(c *gin.Context) {
-
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var orderItem models.OrderItem
 	}
 }
