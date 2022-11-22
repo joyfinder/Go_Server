240698e27c03d1bf3e57ac1b04e68bbbfc007d90
Ta-Liang Lu <ste176876@gmail.com>
2022-11-22 20:45:59 +0800

    Refactor lookupOrderStage

--- Go_restaurant/controllers/orderItemsController.go
+++ Go_restaurant/controllers/orderItemsController.go
@@ -103,7 +103,8 @@ func ItemsByOrder(id string) (OrderItems []primitive.M, err error) {
 	lookupStage := bson.D{{"$lookup"}, bson.D{{"from","food"}, {"localField","food_id"}, {"foreignField","food_id"}, {"as","food"}}}
 	unwindStage :=  bson.D{{"$unwind"}, bson.D{{"path","$food"}, {"preserveNullAndEmptyArrays",true}}}} 
 
-	lookupOrderStage := bson.D{{"$lookup", bson.D{{"food","order"}}}}
+	lookupOrderStage := bson.D{{"$lookup", bson.D{{"food","order"}, {"localField", "order_id"}, {"foreignField", "order_id"}, {"as","order_id"}}}}
+	
 }
 
 func UpdateOrderItem() gin.HandlerFunc {
